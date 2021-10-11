105e5698b58dd98438bc65dd53334823faadc374
tarikyavuzca <tarikyavuzca@gmail.com>
2021-10-11 18:08:38 -0400

    JOIN operations completed

---
+++ 3_queries_group-by/2_larger_cohorts.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as cohort_name, COUNT(students.*) as student_count
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohort_name
+HAVING COUNT(students.*) >= 18
+ORDER BY student_count;
